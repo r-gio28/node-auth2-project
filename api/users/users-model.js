e07eb34c058dcef23ce60c0c56e8d64fd2a7834d
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-14 17:08:24 -0600

    register endpoint is working

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -46,6 +46,21 @@ function findBy(filter) {
       }
     ]
    */
+  /*
+      select 
+          user_id,
+          username,
+          password,
+          role_name
+      from users
+      join roles on
+          users.role_id = roles.role_id
+      where users.user_id = 1;
+  */
+      return db('users')
+      .join('roles', 'users.role_id', 'roles.role_id')
+      .select('user_id', 'username', 'password', 'role_name')
+      .where(filter)
 }
 
 function findById(user_id) {
@@ -59,6 +74,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
