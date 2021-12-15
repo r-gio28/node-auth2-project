7d688fdde2f9e21cdce09fba3c4a774486df3d0d
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-14 18:43:36 -0600

    succesfully logging existing user in

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,5 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
-
+const { findBy } = require('../users/users-model')
 const restricted = (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header:
@@ -16,7 +16,7 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
- next()
+   
 }
 
 const only = role_name => (req, res, next) => {
@@ -34,7 +34,7 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -42,7 +42,17 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
-    next()
+    try {
+      const [user] = await findBy({ username: req.body.username })
+      if(!user) {
+        next({status: 422, message: 'Invalid credentials'})
+      } else {
+        req.user = user
+        next()
+      }
+    } catch (err) {
+      next(err)
+    }
 }
 
 
